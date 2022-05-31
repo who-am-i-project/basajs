1814240ae2243642a21c460d83e8a91b89c63134
Archy <archy.revera@protonmail.com>
2022-05-31 14:03:36 +0300

    Add basic code for scoreboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+// Write react application that creates a scoreboard
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 0,
+      highScore: 0,
+      message: 'Click to start',
+      color: 'black',
+      backgroundColor: 'white'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+      score: this.state.score + 1,
+      message: 'Good job!',
+      color: 'green',
+      backgroundColor: 'lightgreen'
+    });
+    if (this.state.score > this.state.highScore) {
+      this.setState({
+        highScore: this.state.score,
+        message: 'New high score!',
+        color: 'green',
+        backgroundColor: 'lightgreen'
+      });
+    }
+  }
+  render() {
+    const scoreboard = require('./scoreboard.json');
+    let scoreboardInfo = JSON.parse(JSON.stringify(scoreboard));
+    let scoreboardItems = scoreboardInfo.scoreboardItems;
 
-export default App;
+    scoreboardItems.sort(function(a, b) {
+      return b.score - a.score;
+    });
+
+    let scoreboardDivs = scoreboardItems.map(function(item, index) {
+      if (index < 5)
+        return (
+          <div key={index}>
+            <div>
+              {item.name}: <span>{item.score}</span>
+            </div>
+          </div>
+        );
+      else
+        return null;
+    });
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <div class="Scoreboard-wrapper">
+            <h2>Scoreboard</h2>
+            <div className="Scoreboard">
+              <div className="Scoreboard-header">
+                <div>
+                  Name <span>Score</span>
+                </div>
+              </div>
+              <div className="Scoreboard-body">{scoreboardDivs}</div>
+            </div>
+          </div>
+          <div class="NewsAndEvent-wrapper">
+            <h2>News and Event</h2>
+            <div className="NewsAndEvent">
+              <ul>
+                <li>
+                  <b>News1 Title: </b>
+                  <span>News1 Description</span>
+                </li>
+                <li>
+                  <b>News2 Title: </b>
+                  <span>News2 Description</span>
+                </li>
+                <li>
+                  <b>Event1 Title: </b>
+                  <span>Event1 Description</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+} export default App;
