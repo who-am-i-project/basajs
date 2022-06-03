d54024787db6221919689ca22b042469aa4e7a99
vityha <viktor.povazhuk@ucu.edu.ua>
2022-06-03 15:50:39 +0300

    rewrote lobby logic

--- backend/user_input.js
+++
@@ -1,9 +0,0 @@
-export class UserInput {
-    constructor(userId, text, type) {
-        this.userId = userId;
-        this.text = text;
-        this.type = type;
-        this.positive = 0;
-        this.negative = 0;
-    }
-}
